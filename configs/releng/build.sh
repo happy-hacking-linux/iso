1393a06e702bd7ab82667a019bad1776cdd5b983
E.Azer Koçulu <azer@kodfabrik.com>
2018-11-27 00:09:55 +0800

    Sync up with remote

--- configs/releng/build.sh
+++ configs/releng/build.sh
@@ -2,10 +2,10 @@
 
 set -e -u
 
-iso_name=archlinux
-iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <http://www.archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_name=happyhackinglinux
+iso_label="HAPPY_HACKING_$(date +%Y%m)"
+iso_publisher="Happy Hacking Linux <http://www.archlinux.org>"
+iso_application="Happy Hacking Linux Live/Rescue CD"
 iso_version=$(date +%Y.%m.%d)
 install_dir=arch
 work_dir=work
