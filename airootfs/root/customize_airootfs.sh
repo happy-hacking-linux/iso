d2c9d9c0a1ba874e666c27374d3f37ba83e33b51
E.Azer Koçulu <azer@kodfabrik.com>
2018-11-28 01:06:12 +0800

    Delete install service

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -19,5 +19,5 @@ sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
-systemctl enable pacman-init.service choose-mirror.service install.service
+systemctl enable pacman-init.service choose-mirror.service
 systemctl set-default multi-user.target
